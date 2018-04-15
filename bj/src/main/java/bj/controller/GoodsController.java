d06a8338b0db42b143da2245f37bf9d540bf67be
lifoer <dh199489@163.com>
2018-04-16 01:04:15 +0800

    update

--- bj/src/main/java/bj/controller/GoodsController.java
+++ bj/src/main/java/bj/controller/GoodsController.java
@@ -15,53 +15,53 @@ import bj.service.GoodsService;
 
 @Controller
 public class GoodsController {
-	
+
 	@Autowired
 	private GoodsService goodsService;
-	
-	
+
 	@RequestMapping("/index.html")
 	public String index(Model model) {
 		model.addAttribute("checkJd", true);
 		model.addAttribute("checkDe", true);
 		return "index";
 	}
+
 	@RequestMapping("/goods.html")
-	public String queryGoods(String word,String[] shop,String sort,@RequestParam(required = false, defaultValue = "1", value = "pageNum")int pageNum,Model model,HttpSession session) {
-		if(word != null && !word.equals("") && shop != null && shop.length > 0) {
-			//返回商品,分页每页60条
-			PageInfo<Goods> pageInfo = goodsService.updateGoods(word,shop,sort,pageNum,session);
+	public String queryGoods(String word, String[] shop, String sort,
+			@RequestParam(required = false, defaultValue = "1", value = "pageNum") int pageNum, Model model,
+			HttpSession session) {
+		if (word != null && !word.equals("") && shop != null && shop.length > 0) {
+			// 返回商品,分页每页60条
+			PageInfo<Goods> pageInfo = goodsService.updateGoods(word, shop, sort, pageNum, session);
 			model.addAttribute("pageInfo", pageInfo);
-			//返回多选框
+			// 返回多选框
 			boolean checkJd = false;
-			if(shop != null && shop.length > 0) {
-				for (String shopStr : shop) {
-					//是否查询yhd
-					if(shopStr.equals("yhd")) {
-						model.addAttribute("checkYhd", true);
-					}
-					//是否查询jd
-					if(shopStr.equals("jd")) {
-						checkJd = true;
-					}
-					//是否查询dd
-					if(shopStr.equals("dd")) {
-						model.addAttribute("checkDd", true);
-					}
-					//是否查询jmyp
-					if(shopStr.equals("jmyp")) {
-						model.addAttribute("checkJmyp", true);
-					}
+			for (String shopStr : shop) {
+				// 是否查询yhd
+				if (shopStr.equals("yhd")) {
+					model.addAttribute("checkYhd", true);
 				}
-				model.addAttribute("checkJd", checkJd);
+				// 是否查询jd
+				if (shopStr.equals("jd")) {
+					checkJd = true;
+				}
+				// 是否查询dd
+				if (shopStr.equals("dd")) {
+					model.addAttribute("checkDd", true);
+				}
+				// 是否查询jmyp
+				if (shopStr.equals("jmyp")) {
+					model.addAttribute("checkJmyp", true);
+				}
 			}
-			//返回单选框
-			if(sort != null && sort.equals("default")) {
+			model.addAttribute("checkJd", checkJd);
+			// 返回单选框
+			if (sort != null && sort.equals("default")) {
 				model.addAttribute("checkDe", true);
 			} else {
-				model.addAttribute("checkPr",true);
+				model.addAttribute("checkPr", true);
 			}
-			//返回关键字
+			// 返回关键字
 			model.addAttribute("word", word);
 			return "index";
 		}
